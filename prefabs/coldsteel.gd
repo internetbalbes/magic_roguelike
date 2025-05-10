d2cba016af8a4a5bb6d0880790c4c69da62567f3
internetbalbes <internetbalbes@gmail.com>
2025-05-10 16:28:40 +0200

    main 0.3 fix error coldsteel

--- prefabs/coldsteel.gd
+++ prefabs/coldsteel.gd
@@ -6,21 +6,18 @@ extends Node3D
 @onready var timer_find_enemy_in_area = $timer_find_enemy_in_area
 @export var player : CharacterBody3D
 
-#thunderbolt's speed
-var move_speed = 20.0
 # sektor wykrywania (np. 60 stopni)
-var detection_angle_deg_near_enemy = 60.0
-var single_steel_demage = 1
-var splash_steel_demage = 1
+var detection_angle_deg_near_enemy = 40.0
+var single_steel_damage = 1
+var splash_steel_damage = 1
 
 func _ready()->void:
 	var config = ConfigFile.new()
 	if config.load("res://settings.cfg") == OK:
 		detection_angle_deg_near_enemy =  config.get_value("player_coldsteel", "detection_angle_deg_near_enemy", detection_angle_deg_near_enemy)
-		move_speed = config.get_value("player_coldsteel", "move_speed", move_speed)
 		collision_near_enemy.shape.radius = config.get_value("player_coldsteel", "detection_distance_near_enemy", 1.5)
-		single_steel_demage = config.get_value("player_coldsteel", "single_steel_demage", single_steel_demage)
-		splash_steel_demage = config.get_value("player_coldsteel", "splash_steel_demage", splash_steel_demage)
+		single_steel_damage = config.get_value("player_coldsteel", "single_steel_damage", single_steel_damage)
+		splash_steel_damage = config.get_value("player_coldsteel", "splash_steel_damage", splash_steel_damage)
 		#config.save("res://settings.cfg")
 	config = null
 	area3d_near_enemy.monitoring = false
@@ -31,7 +28,7 @@ func action_cold_steel(node: Node3D, pos: Vector3, type: String):
 			var distance = global_position.distance_to(pos)
 			if distance < collision_near_enemy.shape.radius:
 				if node.is_in_group("enemy"):
-					node.take_damage("coldsteel", "", single_steel_demage)
+					node.take_damage("coldsteel", "", single_steel_damage)
 				else:
 					node.portal_free()
 		elif type == "splash":
@@ -48,5 +45,5 @@ func _on_timer_find_enemy_in_area_timeout() -> void:
 	var enemies_in_area = area3d_near_enemy.get_overlapping_bodies()	
 	for obj in enemies_in_area:
 		if _check_near_enemy(obj.global_position):
-			obj.take_damage("coldsteel", "", splash_steel_demage)
+			obj.take_damage("coldsteel", "", splash_steel_damage)
 	area3d_near_enemy.emitting = false
